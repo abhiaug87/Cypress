58a93791fe8255a5183de377547e381f457e1baa
abhiaug87 <32320124+abhiaug87@users.noreply.github.com>
2019-11-28 14:16:51 +1300

    Add files via upload

--- landing.js
+++ landing.js
@@ -1,7 +1,7 @@
   export class LandingPage {
      verifyLandingPage = function () {
-        expect(cy.contains('See More...'),"Text is missing");
-        cy.contains('See More...').click();
+        expect(cy.contains('See more...'),"Text is missing");
+        cy.contains('See more...').click();
         setInterval(9000);
       
     }
--- view.js
+++ view.js
@@ -4,7 +4,7 @@ export class ViewProfile{
         cy.contains('Abhishek').click();
         expect(cy.contains('Abhishek Kulkarni'),"Text is missing");
         expect(cy.contains('Edit Profile'),"Text is missing");
-        expect(cy.contains('Activity Log'),"Text is missing");
+        expect(cy.contains('Activity log'),"Text is missing");
         expect(cy.contains('Studied at AUT - Auckland University of Technology'),"Text is missing");
         expect(cy.contains('Went to St Mary'),"Text is missing");
         expect(cy.contains('Lives in Auckland, New Zealand'),"Text is missing");
