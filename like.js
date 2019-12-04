7aa18ab796bb674f25ab87529363b63a0cea5a79
abhiaug87 <32320124+abhiaug87@users.noreply.github.com>
2019-12-04 15:13:50 +1300

    Update like.js

--- like.js
+++ like.js
@@ -3,7 +3,8 @@ export class Like{
         expect(cy.contains('Like'),"Text is missing");
         expect(cy.contains('Follow'),"Text is missing");
         expect(cy.contains('Share'),"Text is missing");
-        cy.get('button.likeButton').contains('Like').click({ position: 'center' }).click({ force: true });
+        cy.get('button.likeButton').contains('Like').click({ position: 'center' });
+        cy.wrap().click({ force: true });
         setInterval(6000);
         expect(cy.contains('Liked'),"Text is missing");
     }
