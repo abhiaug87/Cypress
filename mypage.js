cea1660a55e706ce920d830cb6a14ae3380ee94c
abhiaug87 <32320124+abhiaug87@users.noreply.github.com>
2019-12-04 21:34:29 +1300

    Update mypage.js

--- mypage.js
+++ mypage.js
@@ -1,6 +1,10 @@
 export class MyPage{
     clickmypage = function () {
-        expect(cy.contains('Testing page'),"Text is missing");
+        expect(cy.contains('Notifications'),"Text is missing");
+        cy.contains('Notifications').click({ force: true });
+        expect(cy.contains('Notifications'),"Text is missing");
+        setInterval(9000);
         cy.contains('Testing page').click();
+        setInterval(9000);
     }
 }
