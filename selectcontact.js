13069cde272065e164b24f59f7b5851c8c165812
abhiaug87 <32320124+abhiaug87@users.noreply.github.com>
2019-12-09 17:16:04 +1300

    Add files via upload

--- selectcontact.js
+++ selectcontact.js
@@ -1,6 +1,7 @@
 export class SelectContact{
     selectingcontact = function () {
-       cy.contains('Atinder Bal').click({position: 'bottomLeft', force: true});
+        setInterval(9000);
+       cy.contains('This is a test').trigger('mouseover').click({position: 'left', force: true});
        setInterval(9000);
        expect( cy.get('div._1mf._1mj'),"Element is missing");
        cy.get('div._1mf._1mj').click().type('This is a test').type('{enter}');  
