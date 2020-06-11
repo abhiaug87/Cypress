b3beaa4eff4175f97a65ad1279001c84876b546d
abhiaug87 <32320124+abhiaug87@users.noreply.github.com>
2020-06-11 20:19:41 +1200

    Add files via upload

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
   export class LoginPage 
   {
    enterLoginCredentials = function () {
-      if(cy.contains('Email or Phone'))
+      if(cy.contains('Email or phone'))
       {
-     expect(cy.contains('Email or Phone'),"Text is missing");
+     expect(cy.contains('Email or phone'),"Text is missing");
      expect(cy.contains('Password'),"Text is missing");
-     expect(cy.contains('Email or Phone').click().type('username'));
+     expect(cy.contains('Email or phone').click().type('kulkarniabhishek23887@gmail.com'));
      
      }
       else if(cy.contains('Abhishek'))
@@ -18,16 +18,16 @@
      else if(cy.contains('Email address or phone number')) 
      {
      expect(cy.get('text').contains('Email address or phone number'),"Text is missing");
-     expect(cy.get('text').contains('Email address or phone number').click({ force:true }).type('username'));
+     expect(cy.get('text').contains('Email address or phone number').click({ force:true }).type('kulkarniabhishek23887@gmail.com'));
      expect(cy.get('text').contains('Password'),"Text is missing");
-     expect(cy.get('text').contains('Password').click({ force:true }).type('password'));
+     expect(cy.get('text').contains('Password').click({ force:true }).type('Testmycode123'));
   
     }
  }
 
    submitCredentials = function () {
     expect(cy.contains('Log In'),"Text is missing");
-    expect(cy.contains('Password').click().type('password'));
+    expect(cy.contains('Password').click().type('Testmycode123'));
     expect(cy.contains('Log In').click());
    }
 }
